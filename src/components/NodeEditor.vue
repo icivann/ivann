be45f0472b0799031627c0e4be06cc09e0e8ea23
Gidon <gidonkatten@gmail.com>
2020-10-14 19:16:27 +0100

    Created nodes used in mnist example

--- src/components/NodeEditor.vue
+++ src/components/NodeEditor.vue
@@ -1,10 +1,6 @@
 <template>
-  <div>
-    This is the Node editor
-    <div class="node-editor">
-      <baklava-editor :plugin="viewPlugin"></baklava-editor>
-    </div>
-    after baklava
+  <div class="node-editor">
+    <baklava-editor :plugin="viewPlugin"></baklava-editor>
   </div>
 </template>
 
@@ -13,11 +9,14 @@ import { Component, Prop, Vue } from 'vue-property-decorator';
 import { Editor } from '@baklavajs/core';
 import { OptionPlugin } from '@baklavajs/plugin-options-vue';
 import { ViewPlugin } from '@baklavajs/plugin-renderer-vue';
+import { Layers, Nodes } from '@/nodes/model/Types';
 
 // Importing the nodes
 import Conv2D from '@/nodes/model/conv/Conv2D';
-import MaxPooling2D from '@/nodes/model/pool/MaxPooling2D';
+import MaxPool2D from '@/nodes/model/pool/MaxPool2D';
 import Dense from '@/nodes/model/linear/Dense';
+import Flatten from '@/nodes/model/reshape/Flatten';
+import Dropout from '@/nodes/model/regularization/Dropout';
 
 @Component
 export default class NodeEditor extends Vue {
@@ -32,16 +31,16 @@ export default class NodeEditor extends Vue {
     this.editor.use(this.viewPlugin);
 
     // Model Layer Nodes
-    this.editor.registerNodeType('Dense', Dense);
-    this.editor.registerNodeType('Conv2D', Conv2D, 'Convolution Layers');
-    this.editor.registerNodeType('MaxPooling2D', MaxPooling2D, 'Pooling Layers');
+    this.editor.registerNodeType(Nodes.Dense, Dense, Layers.Linear);
+    this.editor.registerNodeType(Nodes.Conv2D, Conv2D, Layers.Conv);
+    this.editor.registerNodeType(Nodes.MaxPool2D, MaxPool2D, Layers.Pool);
+    this.editor.registerNodeType(Nodes.Dropout, Dropout, Layers.Regularization);
+    this.editor.registerNodeType(Nodes.Flatten, Flatten, Layers.Reshape);
   }
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-
 .node-editor{
   width: 100%;
   height: 500px;
---
+++ src/nodes/model/Types.ts
@@ -0,0 +1,15 @@
+export const enum Layers {
+  Linear = 'Linear',
+  Conv = 'Convolution',
+  Pool = 'Pooling',
+  Regularization = 'Regularization',
+  Reshape = 'Reshaping',
+}
+
+export const enum Nodes {
+  Dense = 'Dense',
+  Conv2D = 'Convolution2D',
+  MaxPool2D = 'MaxPooling2D',
+  Dropout = 'Dropout',
+  Flatten = 'Flatten'
+}
