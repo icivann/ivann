654c593dac62078bf8ca936dbd6641b2e092e339
Nico D'Cotta <nico.dc@outlook.com>
2020-10-15 19:45:27 +0100

    fix linting

--- src/app/ir/Graphs.ts
+++ src/app/ir/Graphs.ts
@@ -1,7 +1,6 @@
 import GraphNode from '@/app/ir/GraphNode';
 import Conv2D from '@/app/ir/Conv2D';
 import { UUID } from '@/app/util';
-import { MaxPool2D } from '@/app/ir/maxPool';
 import {
   BuiltinActivationF,
   BuiltinInitializer,
@@ -10,21 +9,30 @@ import {
   Padding,
   Regularizer,
 } from '@/app/ir/irCommon';
+import MaxPool2D from '@/app/ir/maxPool2D';
 
-export namespace Graphs {
-    export function mnist(): GraphNode[] {
-      const defaultWeights: [Initializer, Regularizer] = [BuiltinInitializer.Zeroes, BuiltinRegularizer.None];
-      const conv = new Conv2D(
-        new Set(), 32n, Padding.Same, defaultWeights, null, new UUID('theInNode'), BuiltinActivationF.Relu, [28n, 28n], [2n, 2n],
-      );
-      const convGraph = new GraphNode(conv);
+export default function mnist(): GraphNode[] {
+  const zs = BuiltinInitializer.Zeroes;
+  const none = BuiltinRegularizer.None;
+  const defaultWeights: [Initializer, Regularizer] = [zs, none];
+  const conv = new Conv2D(
+    new Set(),
+    32n,
+    Padding.Same,
+    defaultWeights,
+    null,
+    new UUID('theInNode'),
+    BuiltinActivationF.Relu,
+    [28n, 28n],
+    [2n, 2n],
+  );
+  const convGraph = new GraphNode(conv);
 
-      const maxPool = new MaxPool2D(
-        new Set(), convGraph.uniqueId, Padding.Same, [28n, 28n], [2n, 2n],
-      );
-      const maxPoolGraph = new GraphNode(maxPool);
-      conv.outputs.add(maxPoolGraph.uniqueId);
+  const maxPool = new MaxPool2D(
+    new Set(), convGraph.uniqueId, Padding.Same, [28n, 28n], [2n, 2n],
+  );
+  const maxPoolGraph = new GraphNode(maxPool);
+  conv.outputs.add(maxPoolGraph.uniqueId);
 
-      return [convGraph, maxPoolGraph];
-    }
+  return [convGraph, maxPoolGraph];
 }
