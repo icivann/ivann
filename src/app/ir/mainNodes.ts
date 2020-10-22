2891de7b4ff2565e1649ae75280da32ec8b89278
Kacper Kazaniecki <kk518@ic.ac.uk>
2020-10-22 17:29:59 +0100

    adding InModel and OutModel to ModelNode type

--- src/app/ir/mainNodes.ts
+++ src/app/ir/mainNodes.ts
@@ -1,6 +1,8 @@
 import { Conv } from '@/app/ir/conv/Conv';
 import { MaxPool } from '@/app/ir/maxPool/maxPool';
+import InModel from './InModel';
+import OutModel from './OutModel';
 
 export type MlNode = ModelNode
 
-export type ModelNode = Conv | MaxPool
+export type ModelNode = Conv | MaxPool | InModel | OutModel
