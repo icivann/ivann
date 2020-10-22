248db982c0d8733f0e37f52a6159a1375aed5588
William Profit <williamprofit@hotmail.fr>
2020-10-22 17:15:29 +0100

    done

---
+++ tests/unit/app/parser/parser.spec.ts
@@ -0,0 +1,151 @@
+import parse, { ParsedFunction } from '@/app/parser/parser';
+
+describe('parser', () => {
+  it('parses a single function with args correctly', () => {
+    const text = 'def func(arg1, arg2):\n'
+      + '  line1\n'
+      + '  line2\n';
+
+    const expected = [
+      new ParsedFunction(
+        'func',
+        '  line1\n  line2\n',
+        ['arg1', 'arg2'],
+      ),
+    ];
+
+    return parse(text).then((res) => {
+      expect(res).toEqual(expected);
+    });
+  });
+
+  it('parses a single function without args correctly', () => {
+    const text = 'def func():\n'
+      + '  line1\n'
+      + '  line2\n';
+
+    const expected = [
+      new ParsedFunction(
+        'func',
+        '  line1\n  line2\n',
+        [],
+      ),
+    ];
+
+    return parse(text).then((res) => {
+      expect(res).toEqual(expected);
+    });
+  });
+
+  it('parses a tab indent correctly', () => {
+    const text = 'def func():\n'
+      + '\tline1\n'
+      + '\tline2\n';
+
+    const expected = [
+      new ParsedFunction(
+        'func',
+        '\tline1\n\tline2\n',
+        [],
+      ),
+    ];
+
+    return parse(text).then((res) => {
+      expect(res).toEqual(expected);
+    });
+  });
+
+  it('parses two functions correctly', () => {
+    const text = 'def func():\n'
+      + '\tline1\n'
+      + '\tline2\n'
+      + 'line3\n'
+      + 'def func2(arg):\n'
+      + '\tline4\n'
+      + '\tline5\n'
+      + 'line6\n';
+
+    const expected = [
+      new ParsedFunction(
+        'func',
+        '\tline1\n\tline2\n',
+        [],
+      ),
+      new ParsedFunction(
+        'func2',
+        '\tline4\n\tline5\n',
+        ['arg'],
+      ),
+    ];
+
+    return parse(text).then((res) => {
+      expect(res).toEqual(expected);
+    });
+  });
+
+  it('parses a function with multiple levels of indentation', () => {
+    const text = 'def func():\n'
+      + '  line1\n'
+      + '    line2\n'
+      + '  line3\n';
+
+    const expected = [
+      new ParsedFunction(
+        'func',
+        '  line1\n    line2\n  line3\n',
+        [],
+      ),
+    ];
+
+    return parse(text).then((res) => {
+      expect(res).toEqual(expected);
+    });
+  });
+  it('fails to parse a non file with no functions', () => {
+    const text = 'line1\n'
+      + '  line2\n'
+      + '  line3\n';
+
+    expect(parse(text)).rejects.toThrowError();
+  });
+
+  it('fails to parse a file with a badly indented function', () => {
+    const text = 'line1\n'
+      + '  def func(arg1):\n'
+      + '    line3\n';
+
+    expect(parse(text)).rejects.toThrowError();
+  });
+
+  it('fails to parse a badly formatted function signature 1', () => {
+    const text = 'def funcarg1, arg2):\n'
+      + '  line2\n'
+      + '  line3\n';
+
+    expect(parse(text)).rejects.toThrowError();
+  });
+
+  it('fails to parse a badly formatted function signature 2', () => {
+    const text = 'def func(arg1, arg2:\n'
+      + '  line2\n'
+      + '  line3\n';
+
+    expect(parse(text)).rejects.toThrowError();
+  });
+
+  it('fails to parse a badly formatted function signature 3', () => {
+    const text = 'def func(arg1, arg2)\n'
+      + '  line2\n'
+      + '  line3\n';
+
+    expect(parse(text)).rejects.toThrowError();
+  });
+
+  it('fails to parse a badly formatted function signature 4', () => {
+    const text = 'deffunc(arg1, arg2)\n'
+      + '  line2\n'
+      + '  line3\n';
+
+    expect(parse(text)).rejects.toThrowError();
+  });
+});
