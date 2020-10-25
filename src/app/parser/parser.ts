969bc4eadd058de69d0ca57cc57ee17827d7d96f
William Profit <williamprofit@hotmail.fr>
2020-10-25 09:53:51 +0000

    use classic function syntax

--- src/app/parser/parser.ts
+++ src/app/parser/parser.ts
@@ -21,7 +21,7 @@ class FunctionSignature {
  * Given a Python file, returns the indentation character sequence
  * @param file file to get the indentation for
  */
-const getIndentation = (file: string): string | null => {
+function getIndentation(file: string): string | null {
   const lines = file.split('\n');
 
   for (let i = 0; i < lines.length; i += 1) {
@@ -39,12 +39,17 @@ const getIndentation = (file: string): string | null => {
   }
 
   return null;
-};
+}
 
-const isLineFunctionDefinition = (line: string): boolean => line.startsWith('def ');
-const isLineIndented = (line: string, indentation: string): boolean => line.startsWith(indentation);
+function isLineFunctionDefinition(line: string): boolean {
+  return line.startsWith('def ');
+}
 
-const parseFunctionSignature = (line: string): FunctionSignature | null => {
+function isLineIndented(line: string, indentation: string): boolean {
+  return line.startsWith(indentation);
+}
+
+function parseFunctionSignature(line: string): FunctionSignature | null {
   if (!line.startsWith('def ') || !line.includes('(') || !line.includes(')') || !line.includes(':')) {
     return null;
   }
@@ -57,69 +62,71 @@ const parseFunctionSignature = (line: string): FunctionSignature | null => {
     name,
     args.length > 0 ? args.split(',').map((it) => it.trim()) : [],
   );
-};
+}
 
-const parse = (str: string): Promise<ParsedFunction[]> => new Promise((resolve, reject) => {
-  const indentation = getIndentation(str);
+function parse(str: string): Promise<ParsedFunction[]> {
+  return new Promise((resolve, reject) => {
+    const indentation = getIndentation(str);
 
-  // If we failed to get the indentation level
-  if (indentation === null) {
-    reject(new Error('badly indented file'));
-    return;
-  }
+    // If we failed to get the indentation level
+    if (indentation === null) {
+      reject(new Error('badly indented file'));
+      return;
+    }
 
-  const lines = str.split('\n');
+    const lines = str.split('\n');
 
-  let isParsingFunction = false;
-  let currentSignature: FunctionSignature | null = null;
-  let currentBody: string | null = null;
+    let isParsingFunction = false;
+    let currentSignature: FunctionSignature | null = null;
+    let currentBody: string | null = null;
 
-  const parsedFunctions = new Array<ParsedFunction>();
+    const parsedFunctions = new Array<ParsedFunction>();
 
-  for (let i = 0; i < lines.length; i += 1) {
-    const line = lines[i];
+    for (let i = 0; i < lines.length; i += 1) {
+      const line = lines[i];
 
-    if (isParsingFunction) {
-      if (isLineIndented(line, indentation)) {
-        if (!isParsingFunction) {
-          reject(new Error(`unexpected indentation at line ${i}: ${line}`));
+      if (isParsingFunction) {
+        if (isLineIndented(line, indentation)) {
+          if (!isParsingFunction) {
+            reject(new Error(`unexpected indentation at line ${i}: ${line}`));
+          } else {
+            // Parsing function body
+            currentBody += `${line}\n`;
+          }
         } else {
-          // Parsing function body
-          currentBody += `${line}\n`;
+          // Finished parsing function
+          parsedFunctions.push(new ParsedFunction(
+            currentSignature!.name,
+            currentBody!,
+            currentSignature!.args,
+          ));
+
+          isParsingFunction = false;
+          currentSignature = null;
+          currentSignature = null;
         }
       } else {
-        // Finished parsing function
-        parsedFunctions.push(new ParsedFunction(
-          currentSignature!.name,
-          currentBody!,
-          currentSignature!.args,
-        ));
+        if (isLineFunctionDefinition(line)) {
+          const signature = parseFunctionSignature(line);
 
-        isParsingFunction = false;
-        currentSignature = null;
-        currentSignature = null;
-      }
-    } else {
-      if (isLineFunctionDefinition(line)) {
-        const signature = parseFunctionSignature(line);
+          if (signature === null) {
+            reject(new Error(`failed to parse function definition at line ${i}: ${line}`));
+            return;
+          }
 
-        if (signature === null) {
-          reject(new Error(`failed to parse function definition at line ${i}: ${line}`));
-          return;
+          isParsingFunction = true;
+          currentSignature = signature;
+          currentBody = '';
         }
 
-        isParsingFunction = true;
-        currentSignature = signature;
-        currentBody = '';
-      }
-
-      if (isLineIndented(line, indentation)) {
-        reject(new Error(`unexpected indentation at line ${i}: ${line}`));
+        if (isLineIndented(line, indentation)) {
+          reject(new Error(`unexpected indentation at line ${i}: ${line}`));
+        }
       }
     }
-  }
 
-  resolve(parsedFunctions);
-});
+    resolve(parsedFunctions);
+  });
+}
 
 export default parse;
