019a7db6792edc45bb1e647582b2e97b0555f3ac
Thomas-Paul Roberts <tomproberts@outlook.com>
2020-10-25 09:44:06 +0000

    Implemented optional minimum and maximum values for integer inputs.

--- src/nodes/model/conv/Conv2D.ts
+++ src/nodes/model/conv/Conv2D.ts
@@ -17,7 +17,9 @@ export default class Conv2D extends Node {
     this.addInputInterface('Input');
     this.addOutputInterface('Output');
 
-    this.addOption('Filters', 'IntOption', 1);
+    this.addOption('Filters', 'IntOption', 1, undefined, {
+      min: 1,
+    });
 
     // TODO: Keras+Pytorch allow shortcut for specifying single int for all dimensions
     this.addOption('Kernel Size', 'VectorOption', [1, 1]);
