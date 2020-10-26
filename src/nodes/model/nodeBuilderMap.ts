baed8b414a91150e0e32e03e82ea57b09e083062
William Profit <williamprofit@hotmail.fr>
2020-10-26 19:08:24 +0000

    fix master (again)

--- src/nodes/model/nodeBuilderMap.ts
+++ src/nodes/model/nodeBuilderMap.ts
@@ -1,4 +1,4 @@
-import { MlNode } from '@/app/ir/mainNodes';
+import { MlNode, ModelNode } from '@/app/ir/mainNodes';
 import Conv2D from '@/app/ir/conv/Conv2D';
 import MaxPool2D from '@/app/ir/maxPool/maxPool2D';
 import Custom from '@/app/ir/Custom';
@@ -7,5 +7,5 @@ type Options = Map<string, any>
 export const nodeBuilder: Map<string, (r: Options) => MlNode> = new Map([
   ['Convolution2D', Conv2D.build],
   ['MaxPooling2D', MaxPool2D.build],
-  ['Custom', Custom.build],
+  ['Custom', Custom.build as (r: Options) => ModelNode],
 ]);
