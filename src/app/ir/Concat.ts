8d7fb770967c5acf8d4b5ea6536855f7cd28734d
Kacper Kazaniecki <kk518@ic.ac.uk>
2020-10-28 21:07:39 +0000

    adding concat node

---
+++ src/app/ir/Concat.ts
@@ -0,0 +1,5 @@
+class Concat {
+  constructor() {}
+}
+
+export default Concat;
--- src/app/ir/mainNodes.ts
+++ src/app/ir/mainNodes.ts
@@ -3,9 +3,12 @@ import { MaxPool } from '@/app/ir/maxPool/maxPool';
 import Custom from '@/app/ir/Custom';
 import InModel from './InModel';
 import OutModel from './OutModel';
+import Concat from './Concat';
 
 export type MlNode = ModelNode
 
 export type ModelLayerNode = Conv | MaxPool
 
-export type ModelNode = ModelLayerNode | InModel | OutModel | Custom
+export type Operations = Concat
+
+export type ModelNode = ModelLayerNode | InModel | OutModel | Custom | Operations
