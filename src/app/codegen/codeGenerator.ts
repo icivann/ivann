2ed96babb819f00fe573b9ef82a64a573d8746da
Kacper Kazaniecki <kk518@ic.ac.uk>
2020-10-29 01:47:56 +0000

    sketching out the DFS recursive traversal for code generation

--- src/app/codegen/codeGenerator.ts
+++ src/app/codegen/codeGenerator.ts
@@ -10,11 +10,12 @@ import {
   Regularizer,
 } from '@/app/ir/irCommon';
 import MaxPool2D from '@/app/ir/maxPool/maxPool2D';
-import { Stack } from 'stack-typescript';
 import { ModelLayerNode } from '@/app/ir/mainNodes';
 import InModel from '../ir/InModel';
 
 import OutModel from '../ir/OutModel';
+import Graph from '../ir/Graph';
+import Concat from '../ir/Concat';
 
 const imports = [
   'import torch',
@@ -48,88 +49,99 @@ function getBrachVar(branchCounter: number): string {
   return (branchCounter === 0) ? 'x' : `x_${branchCounter}`;
 }
 
-function generateModel(nodes: Set<GraphNode>, connections: Map<GraphNode, [GraphNode]>): string {
+// returns:
+//    code lines generated as array of strings
+//    highet branch number it finished on
+function generateModelGraphCode(node: GraphNode, incomingBranch: number, branchesMap:
+  Map<GraphNode, number[]>, graph: Graph, outputs: Set<string>):
+  [string[], number] {
+  let currentBranch = incomingBranch;
+  const nodeName = getNodeName(node);
+
+  let code: string[] = [];
+
+  // TODO: check here for other branching nodes e.g. Concat
+  if (node.modelNode instanceof InModel) {
+    if (currentBranch !== 0) { currentBranch += 1; }
+    code.push(`${getBrachVar(currentBranch)} = ${nodeName}`);
+  } else if (node.modelNode instanceof OutModel) {
+    outputs.add(getBrachVar(currentBranch));
+  } else if (node.modelNode instanceof Concat) {
+    // TODO: test this
+    const readyConnections: number[] = branchesMap.get(node) ?? [];
+    if (readyConnections.length !== node.inputInterfaces.size) {
+      const incomingBranches = branchesMap.get(node);
+      if (incomingBranches !== undefined) {
+        incomingBranches.push(currentBranch);
+        branchesMap.set(node, incomingBranches);
+      }
+      return [code, currentBranch];
+    }
+    const params = readyConnections.map((branch) => getBrachVar(branch)).join(',');
+    code.push(`${getBrachVar(currentBranch)} = self.${nodeName}(${params})`);
+  } else {
+    code.push(`${getBrachVar(currentBranch)} = self.${nodeName}(${getBrachVar(currentBranch)})`);
+  }
+
+  graph.nextNodesFrom(node).forEach((n) => {
+    const incomingBranches = branchesMap.get(n);
+    if (incomingBranches !== undefined) {
+      incomingBranches.push(1);
+      branchesMap.set(n, incomingBranches);
+    } else {
+      branchesMap.set(n, [currentBranch]);
+    }
+    const [retCode, retBranch] = generateModelGraphCode(n, currentBranch, branchesMap, graph,
+      outputs);
+    currentBranch = Math.max(currentBranch, retBranch);
+    code = code.concat(retCode);
+  });
+
+  return [code, currentBranch];
+}
+
+function generateModel(graph: Graph): string {
   const header = 'class Model(nn.Module):';
 
   // resetting the naming counters for each new graph
   nodeNames = new Map<GraphNode, string>();
   nodeTypeCounters = new Map<string, number>();
-  const nodeArr: Array<GraphNode> = [...nodes];
+  const outputs: Set<string> = new Set();
 
-  const inputs = nodeArr.filter((item: GraphNode) => item.modelNode instanceof InModel);
-  const outputs: string[] = [];
+  const inputs = graph.nodesAsArray.filter((item: GraphNode) => item.modelNode instanceof InModel);
   const inputNames = inputs.map((node) => getNodeName(node));
-  const forward: string[] = [`${indent}def forward(self, ${inputNames.join(', ')})`];
+  let forward: string[] = [`${indent}def forward(self, ${inputNames.join(', ')})`];
 
-  const stack = new Stack<GraphNode>(...inputs);
+  // dummy GraphNode to start off the recusrive DFS traversal
+  // const startLayer = new InModel([0n]);
+  // const startNode = new GraphNode(startLayer, new UUID('startNode'));
+  // connections.set(startNode, inputs);
+  let currentBranch = 0;
+  const branchesMap = new Map<GraphNode, number[]>();
 
-  let branchCounter = 0;
-  while (stack.length > 0) {
-    const node = stack.pop();
-    const nodeName = getNodeName(node);
-    // TODO: check here for other branching nodes
-    if (node.modelNode instanceof InModel) {
-      if (branchCounter !== 0) { branchCounter += 1; }
-      forward.push(`${getBrachVar(branchCounter)} = ${nodeName}`);
-    } else if (node.modelNode instanceof OutModel) {
-      outputs.push(getBrachVar(branchCounter));
-    } else {
-      forward.push(`${getBrachVar(branchCounter)} = self.${nodeName}(${getBrachVar(branchCounter)})`);
-    }
-
-    const nodeConnections = connections.get(node);
-
-    if (nodeConnections !== undefined) {
-      const makeNewBranches = (nodeConnections.length > 1);
-      nodeConnections.forEach((outNode) => {
-        stack.push(outNode);
-      });
-    }
-  }
+  inputs.forEach((n) => {
+    const [retCode, retBranch] = generateModelGraphCode(n, currentBranch, branchesMap, graph,
+      outputs);
+    currentBranch = Math.max(currentBranch, retBranch);
+    forward = forward.concat(retCode);
+  });
 
   const nodeDefinitions: string[] = [];
-
-  nodeArr.forEach((n) => {
+  graph.nodesAsArray.forEach((n) => {
     if ((n.modelNode as ModelLayerNode).initCode !== undefined) {
       nodeDefinitions.push(`self.${getNodeName(n)} = ${(n.modelNode as ModelLayerNode).initCode()}`);
     }
   });
   const init = [`${indent}def __init__(self):`].concat(nodeDefinitions);
-  forward.push(`return ${outputs.join(', ')}`);
+  forward.push(`return ${[...outputs].join(', ')}`);
   const forwardMethod = forward.join(`\n${indent}${indent}`);
   const initMethod = init.join(`\n${indent}${indent}`);
 
   return [header, initMethod, forwardMethod].join('\n');
 }
 
-export default function generateCode(): string {
-  const input = new InModel([256n, 256n, 3n]);
-  const output = new OutModel();
-
-  const zs = BuiltinInitializer.Zeroes;
-  const none = BuiltinRegularizer.None;
-  const defaultWeights: [Initializer, Regularizer] = [zs, none];
-  const conv = new Conv2D(
-    32n,
-    Padding.Same,
-    defaultWeights,
-    null,
-    BuiltinActivationF.Relu,
-    [28n, 28n],
-    [2n, 2n],
-  );
-  const maxPool = new MaxPool2D(Padding.Same, [28n, 28n], [2n, 2n]);
-
-  const list = [input, conv, maxPool, output].map((t) => new GraphNode(t));
-
-  const nodes = new Set<GraphNode>(list);
-  const connections = new Map<GraphNode, [GraphNode]>([
-    [list[0], [list[1]]],
-    [list[1], [list[2]]],
-    [list[2], [list[3]]],
-  ]);
-
-  const model = generateModel(nodes, connections);
+export default function generateCode(graph: Graph): string {
+  const model = generateModel(graph);
   const result = [imports, model].join('\n\n');
   return result;
 }
