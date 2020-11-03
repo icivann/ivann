bbb119f1fec8a056145e406861d21820b8521ffa
Thomas-Paul Roberts <tomproberts@outlook.com>
2020-11-03 12:51:50 +0000

    Implemented loading overview with IO from file.

--- src/nodes/overview/Model.ts
+++ src/nodes/overview/Model.ts
@@ -3,6 +3,7 @@ import { Overview } from '@/nodes/model/Types';
 import { EditorModel } from '@/store/editors/types';
 import { NodeIOChange } from '@/nodes/overview/EditorIOUtils';
 import { getEditorIOs } from '@/store/editors/utils';
+import { INodeState } from '@baklavajs/core/dist/baklavajs-core/types/state.d';
 
 export default class Model extends Node {
   name = '';
@@ -34,5 +35,30 @@ export default class Model extends Node {
     for (const inputRemoved of inputChange.removed) this.removeInterface(inputRemoved);
     for (const outputAdded of outputChange.added) this.addOutputInterface(outputAdded);
     for (const outputRemoved of outputChange.removed) this.removeInterface(outputRemoved);
+  }
+
+  public load(state: INodeState) {
+    const inputs: string[] = [];
+    const outputs: string[] = [];
+    state.interfaces.forEach(([k, v]) => {
+      if ('isInput' in v) (v.isInput ? inputs : outputs).push(k);
+    });
+    this.updateIO({ added: inputs, removed: [] }, { added: outputs, removed: [] });
+    super.load(state);
+  }
+
+  public save(): INodeState {
+    /* Copied from default Node implementation, but interfaces extended with isInput. */
+    const state: INodeState = {
+      type: this.type,
+      id: this.id,
+      name: this.name,
+      options: Array.from(this.options.entries())
+        .map(([k, o]) => [k, o.value]) as any,
+      state: this.state,
+      interfaces: Array.from(this.interfaces.entries())
+        .map(([k, i]) => [k, { isInput: i.isInput, ...i.save() }]) as any,
+    };
+    return this.hooks.save.execute(state);
   }
 }
