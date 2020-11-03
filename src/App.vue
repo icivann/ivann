8af14aa730cd182eb85e4c2e5a05ca7c32f578a4
Lancelot Blanchard <lancelotblanchard@hotmail.fr>
2020-11-03 12:31:34 +0100

    Add cookie law banner

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,19 @@
 <template>
   <div id="app">
     <router-view/>
+    <cookie-law theme="dark-lime"></cookie-law>
   </div>
 </template>
 
 <script>
-import { Vue } from 'vue-property-decorator';
+import CookieLaw from 'vue-cookie-law-with-type';
+import { Component, Vue } from 'vue-property-decorator';
 
+@Component({
+  components: {
+    CookieLaw,
+  },
+})
 export default class App extends Vue {
 }
 </script>
@@ -18,5 +25,17 @@ export default class App extends Vue {
 }
 #nav a.router-link-exact-active {
   color: #42b983;
+}
+
+.Cookie {
+  background: var(--grey) !important;
+  padding-top: 0.5em !important;
+  padding-bottom: 0.5em !important;
+}
+
+.Cookie__button {
+  background: var(--blue) !important;
+  padding-top: 0.2em !important;
+  padding-bottom: 0.2em !important;
 }
 </style>
