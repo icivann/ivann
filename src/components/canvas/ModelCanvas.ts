9d8b6a6927915ebd52b3dccba236666f6b05a9ad
Kacper Kazaniecki <kk518@ic.ac.uk>
2020-11-08 20:50:53 +0000

    registering dropout nodes

--- src/components/canvas/ModelCanvas.ts
+++ src/components/canvas/ModelCanvas.ts
@@ -12,16 +12,32 @@ import InModel from '@/nodes/model/InModel';
 import Convtranspose1d from '@/nodes/model/Convtranspose1d';
 import Convtranspose2d from '@/nodes/model/Convtranspose2d';
 import Convtranspose3d from '@/nodes/model/Convtranspose3d';
+import Maxpool1d from '@/nodes/model/Maxpool1d';
+import Maxpool2d from '@/nodes/model/Maxpool2d';
+import Maxpool3d from '@/nodes/model/Maxpool3d';
+import Dropout from '@/nodes/model/Dropout';
+import Dropout2d from '@/nodes/model/Dropout2d';
+import Dropout3d from '@/nodes/model/Dropout3d';
+import ReLU from '@/nodes/model/Relu';
 
 export default class ModelCanvas extends AbstractCanvas {
   public registerNodes(editor: Editor): void {
     editor.registerNodeType(Nodes.Conv1D, Conv1d, Layers.Conv);
     editor.registerNodeType(Nodes.Conv2D, Conv2d, Layers.Conv);
     editor.registerNodeType(Nodes.Conv3D, Conv3d, Layers.Conv);
-
     editor.registerNodeType(Nodes.ConvTranspose1d, Convtranspose1d, Layers.Conv);
     editor.registerNodeType(Nodes.ConvTranspose2d, Convtranspose2d, Layers.Conv);
     editor.registerNodeType(Nodes.ConvTranspose3d, Convtranspose3d, Layers.Conv);
+
+    editor.registerNodeType(Nodes.MaxPool1D, Maxpool1d, Layers.Pool);
+    editor.registerNodeType(Nodes.MaxPool2D, Maxpool2d, Layers.Pool);
+    editor.registerNodeType(Nodes.MaxPool3D, Maxpool3d, Layers.Pool);
+
+    editor.registerNodeType(Nodes.Dropout, Dropout, Layers.Dropout);
+    editor.registerNodeType(Nodes.Dropout2d, Dropout2d, Layers.Dropout);
+    editor.registerNodeType(Nodes.Dropout3d, Dropout3d, Layers.Dropout);
+
+    editor.registerNodeType(Nodes.Relu, ReLU, Layers.Activation);
 
     editor.registerNodeType(Nodes.Custom, Custom, Layers.Custom);
     editor.registerNodeType(Nodes.InModel, InModel, Layers.IO);
--- src/nodes/model/nodeBuilderMap.ts
+++ src/nodes/model/nodeBuilderMap.ts
@@ -10,6 +10,10 @@ import ConvTranspose3d from '@/app/ir/model/convtranspose3d';
 import MaxPool1d from '@/app/ir/model/maxpool1d';
 import MaxPool2d from '@/app/ir/model/maxpool2d';
 import MaxPool3d from '@/app/ir/model/maxpool3d';
+import Dropout from '@/app/ir/model/dropout';
+import Dropout2d from '@/app/ir/model/dropout2d';
+import Dropout3d from '@/app/ir/model/dropout3d';
+import ReLU from '@/app/ir/model/relu';
 
 type Options = Map<string, any>
 // eslint-disable-next-line import/prefer-default-export
@@ -20,9 +24,13 @@ export const nodeBuilder: Map<string, (r: Options) => MlNode> = new Map([
   ['ConvTranspose1d', ConvTranspose1d.build],
   ['ConvTranspose2d', ConvTranspose2d.build],
   ['ConvTranspose3d', ConvTranspose3d.build],
-  ['MaxPooling1D', MaxPool1d.build],
-  ['MaxPooling2D', MaxPool2d.build],
-  ['MaxPooling3D', MaxPool3d.build],
+  ['MaxPool1D', MaxPool1d.build],
+  ['MaxPool2D', MaxPool2d.build],
+  ['MaxPool3D', MaxPool3d.build],
+  ['Dropout', Dropout.build],
+  ['Dropout2d', Dropout2d.build],
+  ['Dropout3d', Dropout3d.build],
+  ['Relu', ReLU.build],
   ['Custom', Custom.build as (r: Options) => MlNode],
   ['Concat', Concat.build],
   ['InModel', InModel.build],
