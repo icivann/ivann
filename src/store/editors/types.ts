f703e460d2f694aa5a953ee26b598c52b1e15fb8
Thomas-Paul Roberts <tomproberts@outlook.com>
2020-11-10 23:32:32 +0000

    Removed unnecessary imports in editors/types.ts

--- src/store/editors/types.ts
+++ src/store/editors/types.ts
@@ -1,7 +1,6 @@
 import EditorType from '@/EditorType';
 import { Editor } from '@baklavajs/core';
 import { UUID } from '@/app/util';
-import Custom from '@/nodes/model/custom/Custom';
 
 export interface EditorModel {
   id: UUID;
