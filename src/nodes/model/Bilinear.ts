4837eff4563dd9f4a52cedfdac6f60fd93032134
tas3318 <tas3318@ic.ac.uk>
2020-11-12 15:33:41 +0000

    Change naming for bilinear

---
+++ src/nodes/model/Bilinear.ts
@@ -0,0 +1,25 @@
+import { Node } from '@baklavajs/core';
+import { Layers, Nodes } from '@/nodes/model/Types';
+import { TypeOptions } from '@/nodes/model/BaklavaDisplayTypeOptions';
+import CheckboxValue from '@/baklava/CheckboxValue';
+
+export enum BilinearOptions {
+  In1Features = 'In1 features',
+  In2Features = 'In2 features',
+  OutFeatures = 'Out features',
+  Bias = 'Bias'
+}
+export default class Bilinear extends Node {
+  type = Nodes.Bilinear;
+  name = Nodes.Bilinear;
+
+  constructor() {
+    super();
+    this.addInputInterface('Input');
+    this.addOutputInterface('Output');
+    this.addOption(BilinearOptions.In1Features, TypeOptions.IntOption, 0);
+    this.addOption(BilinearOptions.In2Features, TypeOptions.IntOption, 0);
+    this.addOption(BilinearOptions.OutFeatures, TypeOptions.IntOption, 0);
+    this.addOption(BilinearOptions.Bias, TypeOptions.TickBoxOption, CheckboxValue.CHECKED);
+  }
+}
