095acc64e76968a41719aecd709fe820756f1e29
William Profit <williamprofit@hotmail.fr>
2020-11-20 20:56:16 +0100

    basic codegen for LoadCsv and LoadImages

--- src/app/ir/data/LoadImages.ts
+++ src/app/ir/data/LoadImages.ts
@@ -11,6 +11,19 @@ class LoadImages {
       options.get(nodeName),
     );
   }
+
+  public initCode(name: string): string[] {
+    const path = `${name}_path`;
+    return [
+      `self.${name} = [os.path.join(${path}, file) for file in listdir(${path}) if os.path.isfile(os.path.join(${path}, file))]`,
+    ];
+  }
+
+  public callCode(name: string): string[] {
+    return [
+      `${name} = Image(${name})`,
+    ];
+  }
 }
 
 export default LoadImages;
