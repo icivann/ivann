4272c6d9acb20feb253663800316fa905d52a48d
Nico D'Cotta <nico.dc@outlook.com>
2020-11-24 23:01:05 +0000

    add typecheck for bad adjacent convolution sizes

---
+++ src/app/ir/checking/findBadConvChannels.ts
@@ -0,0 +1,36 @@
+import Graph from '@/app/ir/Graph';
+import Conv1d from '@/app/ir/model/conv1d';
+import Conv2d from '@/app/ir/model/conv2d';
+import Conv3d from '@/app/ir/model/conv3d';
+import GraphNode from '@/app/ir/GraphNode';
+import IrError from '@/app/ir/checking/irError';
+import { Severity } from '@/app/ir/checking/severity';
+
+/**
+ * Finds all nodes in the graph that are right before a Conv node and are Conv
+ * nodes themselves, and checks their channels match.
+ */
+export const findBadConvChannels = (graph: Graph) => {
+  type Conv = Conv1d | Conv2d | Conv3d;
+  const isConv = (node: GraphNode) => node.mlNode instanceof Conv1d
+    || node.mlNode instanceof Conv2d
+    || node.mlNode instanceof Conv3d;
+  return graph.nodesAsArray
+    .filter(isConv)
+    .flatMap((current) => {
+      const currentConv = current.mlNode as Conv;
+      return graph.nextNodesFrom(current)
+        .filter(isConv)
+        .map((n) => [n, n.mlNode] as [GraphNode, Conv])
+        .filter(([_, conv]) => conv.in_channels !== currentConv.out_channels)
+        .map(([offenderNode, offenderConv]) => {
+          const nodes = `${currentConv.name} and ${offenderConv.name}`;
+          const channels = `${currentConv.out_channels} and ${offenderConv.in_channels}`;
+          return new IrError(
+            [offenderNode, current],
+            Severity.Error,
+            `Expected channels of convolutional layers ${nodes} to match, but instead got ${channels}`,
+          );
+        });
+    });
+};
--- tests/unit/app/ir/checking/checking.spec.ts
+++ tests/unit/app/ir/checking/checking.spec.ts
@@ -3,6 +3,7 @@ import { check } from '@/app/ir/checking/check';
 import Graph from '@/app/ir/Graph';
 import IrError from '@/app/ir/checking/irError';
 import Conv2d from '@/app/ir/model/conv2d';
+import Conv1d from '@/app/ir/model/conv1d';
 
 function expectError(graph: Graph, should: (err: IrError) => void) {
   const errors = check(graph);
@@ -25,6 +26,19 @@ describe('type checking', () => {
     const second = outputOrphan.nodesAsArray.filter((n) => n.mlNode instanceof Conv2d)[0];
     expectError(outputOrphan, (err) => {
       expect(err.offenders).toContain(second);
+    });
+  });
+
+  it('bad convoliution channels report errors', () => {
+    const graph = istateToGraph(
+      JSON.parse('{"nodes":[{"type":"InModel","id":"node_16062528273010","name":"i","options":[],"state":{},"interfaces":[["Output",{"id":"ni_16062528273011","value":null}]],"position":{"x":78,"y":158},"width":200,"twoColumn":false},{"type":"OutModel","id":"node_16062528318402","name":"o","options":[],"state":{},"interfaces":[["Input",{"id":"ni_16062528318413","value":null}]],"position":{"x":765,"y":197},"width":200,"twoColumn":false},{"type":"Conv1d","id":"node_160625823243814","name":"Conv1d YOLO","options":[["In channels",0],["Out channels",4],["Kernel size",[0]],["Stride",[2]],["Padding",[0]],["Dilation",[1]],["Groups",1],["Bias",1],["Padding mode","zeros"]],"state":{},"interfaces":[["Input",{"id":"ni_160625823243815","value":null}],["Output",{"id":"ni_160625823243816","value":null}]],"position":{"x":206,"y":420},"width":200,"twoColumn":false},{"type":"Conv2d","id":"node_160625823380817","name":"Conv2d","options":[["In channels",0],["Out channels",3],["Kernel size",[0,0]],["Stride",[1,1]],["Padding",[0,0]],["Dilation",[1,1]],["Groups",1],["Bias",1],["Padding mode","zeros"]],"state":{},"interfaces":[["Input",{"id":"ni_160625823380818","value":null}],["Output",{"id":"ni_160625823380819","value":null}]],"position":{"x":553,"y":420},"width":200,"twoColumn":false}],"connections":[{"id":"160625823620822","from":"ni_16062528273011","to":"ni_160625823243815"},{"id":"160625823870725","from":"ni_160625823243816","to":"ni_160625823380818"},{"id":"160625824086728","from":"ni_160625823380819","to":"ni_16062528318413"}],"panning":{"x":1.4926523584755387,"y":1.657796874732412},"scaling":0.9968341980495156}'),
+    );
+    const conv2d = graph.nodesAsArray.filter((n) => n.mlNode instanceof Conv2d)[0];
+    const conv1d = graph.nodesAsArray.filter((n) => n.mlNode instanceof Conv1d)[0];
+    expectError(graph, (err) => {
+      expect(err.offenders.length).toBe(2);
+      expect(err.offenders).toContain(conv1d);
+      expect(err.offenders).toContain(conv2d);
     });
   });
 });
