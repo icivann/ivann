10bbcb32f3d1899df90aeac6f21b5263936851b9
Nico D'Cotta <nico.dc@outlook.com>
2020-11-26 16:03:08 +0000

    abstarct away adjacency check and add all Conv types to Conv channel check

--- src/app/ir/checking/findBadConvChannels.ts
+++ src/app/ir/checking/findBadConvChannels.ts
@@ -1,42 +1,26 @@
-import Graph from '@/app/ir/Graph';
 import Conv1d from '@/app/ir/model/conv1d';
 import Conv2d from '@/app/ir/model/conv2d';
 import Conv3d from '@/app/ir/model/conv3d';
-import GraphNode from '@/app/ir/GraphNode';
-import IrError from '@/app/ir/checking/irError';
-import { Severity } from '@/app/ir/checking/severity';
 import ConvTranspose1d from '@/app/ir/model/convtranspose1d';
 import ConvTranspose2d from '@/app/ir/model/convtranspose2d';
 import ConvTranspose3d from '@/app/ir/model/convtranspose3d';
+import { adjacencyCheck } from '@/app/ir/checking/adjacencyCheck';
+
+type Conv = Conv1d | Conv2d | Conv3d | ConvTranspose1d | ConvTranspose2d | ConvTranspose3d;
 
 /**
  * Finds all nodes in the graph that are right before a Conv node and are Conv
  * nodes themselves, and checks their channels match.
  */
-export const findBadConvChannels = (graph: Graph) => {
-  type Conv = Conv1d | Conv2d | Conv3d;
-  const isConv = (node: GraphNode) => node.mlNode instanceof Conv1d
+
+export const findBadConvChannels = adjacencyCheck(
+  (node) => node.mlNode instanceof Conv1d
     || node.mlNode instanceof Conv2d
     || node.mlNode instanceof Conv3d
     || node.mlNode instanceof ConvTranspose1d
     || node.mlNode instanceof ConvTranspose2d
-    || node.mlNode instanceof ConvTranspose3d;
-  return graph.nodesAsArray
-    .filter(isConv)
-    .flatMap((current) => {
-      const currentConv = current.mlNode as Conv;
-      return graph.nextNodesFrom(current)
-        .filter(isConv)
-        .map((n) => [n, n.mlNode] as [GraphNode, Conv])
-        .filter(([_, conv]) => conv.in_channels !== currentConv.out_channels)
-        .map(([offenderNode, offenderConv]) => {
-          const nodes = `${currentConv.name} and ${offenderConv.name}`;
-          const channels = `${currentConv.out_channels} and ${offenderConv.in_channels}`;
-          return new IrError(
-            [offenderNode, current],
-            Severity.Error,
-            `Expected channels of convolutional layers ${nodes} to match, but instead got ${channels}`,
-          );
-        });
-    });
-};
+    || node.mlNode instanceof ConvTranspose3d,
+  (fst, snd) => (fst.mlNode as Conv).out_channels === (snd.mlNode as Conv).in_channels,
+  (fst, snd) => `Expected channels of convolutional layers ${fst.mlNode.name} and ${snd.mlNode.name} to match `
+    + `but instead got ${(fst.mlNode as Conv).out_channels} and ${(snd.mlNode as Conv).in_channels}`,
+);
--- tests/unit/app/ir/checking/checking.spec.ts
+++ tests/unit/app/ir/checking/checking.spec.ts
@@ -17,6 +17,7 @@ function expectNoErrors(graph: Graph) {
   const errors = check(graph);
   expect(errors.length).toBe(0);
 }
+
 describe('type checking', () => {
   const outputOrphan = istateToGraph(
     JSON.parse('{"nodes":[{"type":"InModel","id":"node_16040740027440","name":"InModel","options":[],"state":{},"interfaces":[["Output",{"id":"ni_16040740027441","value":null}]],"position":{"x":106.66666666666669,"y":257.6666666666667},"width":200,"twoColumn":false},{"type":"Conv2d","id":"node_16040740160792","name":"Conv2d","options":[["In channels",0],["Out channels",0],["Kernel size",[0,0]],["Stride",[1,1]],["Padding",[0,0]],["Dilation",[1,1]],["Groups",1],["Bias",1],["Padding mode","zeros"]],"state":{},"interfaces":[["Input",{"id":"ni_16040740160803","value":null}],["Output",{"id":"ni_16040740160804","value":null}]],"position":{"x":394.6666666666667,"y":341.6666666666667},"width":200,"twoColumn":false}],"connections":[{"id":"16040740240767","from":"ni_16040740027441","to":"ni_16040740160803"}],"panning":{"x":0,"y":0},"scaling":1}'),
