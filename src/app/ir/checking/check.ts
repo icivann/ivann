5a11b17a59ec368a346e568134b2130d10ffa622
Nico D'Cotta <45274424+Cottand@users.noreply.github.com>
2020-11-26 19:52:42 +0000

    CORE-64 convolution channel out/in check (#106)
    * add typecheck for bad adjacent convolution sizes
    
    * add ConvTransposkd to convolution typecheck
    
    * abstarct away adjacency check and add all Conv types to Conv channel check

--- src/app/ir/checking/check.ts
+++ src/app/ir/checking/check.ts
@@ -1,10 +1,11 @@
 import Graph from '@/app/ir/Graph';
 import IrError from '@/app/ir/checking/irError';
+import { findBadConvChannels } from '@/app/ir/checking/findBadConvChannels';
 import { findDanglingInterfaces } from './findDanglingInterfaces';
 
 export function check(graph: Graph): IrError[] {
   const errorsSoFar = Array<IrError>();
   errorsSoFar.push(...findDanglingInterfaces(graph));
-
+  errorsSoFar.push(...findBadConvChannels(graph));
   return errorsSoFar;
 }
