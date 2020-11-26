4197de5dd1aa29f0fae73cc7dab9b7ecdcaa50ec
Nico D'Cotta <nico.dc@outlook.com>
2020-11-26 15:41:08 +0000

    add ConvTransposkd to convolution typecheck

--- src/app/ir/checking/findBadConvChannels.ts
+++ src/app/ir/checking/findBadConvChannels.ts
@@ -5,6 +5,9 @@ import Conv3d from '@/app/ir/model/conv3d';
 import GraphNode from '@/app/ir/GraphNode';
 import IrError from '@/app/ir/checking/irError';
 import { Severity } from '@/app/ir/checking/severity';
+import ConvTranspose1d from '@/app/ir/model/convtranspose1d';
+import ConvTranspose2d from '@/app/ir/model/convtranspose2d';
+import ConvTranspose3d from '@/app/ir/model/convtranspose3d';
 
 /**
  * Finds all nodes in the graph that are right before a Conv node and are Conv
@@ -14,7 +17,10 @@ export const findBadConvChannels = (graph: Graph) => {
   type Conv = Conv1d | Conv2d | Conv3d;
   const isConv = (node: GraphNode) => node.mlNode instanceof Conv1d
     || node.mlNode instanceof Conv2d
-    || node.mlNode instanceof Conv3d;
+    || node.mlNode instanceof Conv3d
+    || node.mlNode instanceof ConvTranspose1d
+    || node.mlNode instanceof ConvTranspose2d
+    || node.mlNode instanceof ConvTranspose3d;
   return graph.nodesAsArray
     .filter(isConv)
     .flatMap((current) => {
