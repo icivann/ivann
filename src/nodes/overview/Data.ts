0b80d9e7de01077bab07d074e3b460ea42873fb7
William Profit <williamprofit@hotmail.fr>
2020-11-26 23:05:16 +0100

    have data node load and save

--- src/nodes/overview/Data.ts
+++ src/nodes/overview/Data.ts
@@ -3,6 +3,7 @@ import { EditorModel } from '@/store/editors/types';
 import { Node } from '@baklavajs/core';
 import { TypeOptions } from '@/nodes/model/BaklavaDisplayTypeOptions';
 import { getEditorIOs } from '@/store/editors/utils';
+import { INodeState } from '@baklavajs/core/dist/baklavajs-core/types/state.d';
 
 export enum DataOptions {
   BatchSize = 'BatchSize',
@@ -27,5 +28,29 @@ export class Data extends Node {
 
     this.addOption(DataOptions.BatchSize, TypeOptions.IntOption, 32);
     this.addOutputInterface('Dataset');
+  }
+
+  public load(state: INodeState) {
+    state.options.forEach(([k, v]) => {
+      if (k.endsWith('path')) {
+        this.addOption(k, TypeOptions.TextAreaOption, v);
+      }
+    });
+    super.load(state);
+  }
+
+  public save(): INodeState {
+    /* Copied from default Node implementation, but interfaces extended with isInput. */
+    const state: INodeState = {
+      type: this.type,
+      id: this.id,
+      name: this.name,
+      options: Array.from(this.options.entries())
+        .map(([k, o]) => [k, o.value]),
+      state: this.state,
+      interfaces: Array.from(this.interfaces.entries())
+        .map(([k, i]) => [k, { isInput: i.isInput, ...i.save() }]),
+    };
+    return this.hooks.save.execute(state);
   }
 }
